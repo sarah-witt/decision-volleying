f29168857d4bd1f79236adbe6d74f820ff1c1af8
Sarah Witt <witt.s@husky.neu.edu>
2019-07-01 17:17:01 -0400

    Complete volleying logic

--- volleying/models.py
+++ volleying/models.py
@@ -1,3 +1,4 @@
+from django.forms import CheckboxInput, MultipleChoiceField, widgets as w
 from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
@@ -15,7 +16,44 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    pass
+
+    intouchables = models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="The Intouchables (Foreign) – two very different men bond and develop a very close relationship")
+    starfish =  models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="Starfish (Science Fiction) – a young woman struggles with the death of her best friend")
+    versailles = models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="The Queen of Versailles (Documentary) – the economic crisis threatens the fortune of a billionaire family")
+    hush = models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="Hush (Horror) – a deaf writer living in the woods fights for her life when a killer appears in her window")
+    father = models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="Like Father (Rom-com) – a woman left at the altar takes her estranged father on her honeymoon")
+    tomboy = models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="Tomboy (Drama) - a 10-year old girl experiments with with her gender identity over the summer")
+    phoenix = models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="Dark Phoenix (Science Fiction/Action) – the X-Men embark on a risky mission in space")
+    shazam = models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="Shazam! (Superhero) - a 14-year-old transforms into an adult superhero with one magic word")
+    dumbo = models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="Dumbo (Fantasy) - a man and his two children take care of a newborn elephant that can fly")
+    survivalist = models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="The Survivalist (Thriller) - a survivalist hides in the forest protecting his crop from intruders")
+    carol = models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="Carol (Romance) - two women develop a fast bond that becomes a love with complicated consequences")
+    wild = models.BooleanField(blank=True, widget=CheckboxInput, initial=False, label="Wild (Adventure) - a woman's solo undertakes a hike as a way to recover from a recent personal tragedy")
+
+    def get_movies(self):
+        return {'intouchables': self.intouchables, 
+        'starfish': self.starfish,
+        'versailles': self.versailles,
+        'hush': self.hush,
+        'father': self.father,
+        'tomboy': self.tomboy,
+        'phoenix': self.phoenix,
+        'shazam': self.shazam,
+        'dumbo': self.dumbo,
+        'survivalist': self.survivalist,
+        'carol': self.carol,
+        'wild': self.wild}
+
+    def get_remaining_movies(self):
+        return {k for k,v in self.get_movies().items() if not v} 
+
+
+    def volleying(self):
+        #still_available = self.get_movies()
+        return True #any(still_available) and not any(still_available)
+                 
+    
+    isVolleying = models.BooleanField(initial= True)
 
 
 class Player(BasePlayer):
@@ -38,3 +76,6 @@ class Player(BasePlayer):
 
     def get_partner(self):
         return self.get_others_in_group()[0]
+
+    def get_partner_name(self):
+        return self.get_others_in_group()[0].first_name
--- volleying/pages.py
+++ volleying/pages.py
@@ -8,8 +8,9 @@ import random
 
 
 class Introduction(Page):
-    def get_timeout_seconds(self):
-        pass
+    def before_next_page(self):
+        # user has 60 minutes to complete as many pages as possible
+        self.participant.vars['expiry'] = time.time() + 3600
 
 class ParticipantInfo(Page):
     template_name = 'volleying/ParticipantInfo.html'
@@ -23,25 +24,105 @@ class ParticipantInfo(Page):
             return 'Please enter a valid sonaId'
 
 class Instructions(Page):
+    pass
+
+class Chat(Page):
     def get_timeout_seconds(self):
-        pass
+        return 60
 
-class ResultsWaitPage(WaitPage):
+class Volley1(Page):
+    form_model = 'group'
+    template_name = 'volleying/Volley.html'
 
-    def after_all_players_arrive(self):
-        pass
+    def get_form_fields(self): 
+        return self.group.get_remaining_movies()
+
+    def before_next_page(self):
+        if self.timeout_happened:
+            self.player.set_timeout_data()
 
     def is_displayed(self):
-        pass
+        return self.group.isVolleying
 
+class Volley2(Page):
+    form_model = 'group'
+    template_name = 'volleying/Volley.html'
+
+    def get_form_fields(self): 
+        return self.group.get_remaining_movies()
+
+    def before_next_page(self):
+        if self.timeout_happened:
+            self.player.set_timeout_data()
+            
+    def is_displayed(self):
+        return self.group.isVolleying
+
+class Volley3(Page):
+    form_model = 'group'
+    template_name = 'volleying/Volley.html'
+
+    def get_form_fields(self): 
+        return self.group.get_remaining_movies()
+
+    def before_next_page(self):
+        if self.timeout_happened:
+            self.player.set_timeout_data()
+            
+    def is_displayed(self):
+        return self.group.isVolleying
+
+class Volley4(Page):
+    form_model = 'group'
+    template_name = 'volleying/Volley.html'
+
+    def get_form_fields(self): 
+        return self.group.get_remaining_movies()
+
+    def before_next_page(self):
+        if self.timeout_happened:
+            self.player.set_timeout_data()
+            
+    def is_displayed(self):
+        return self.group.isVolleying
+
+class Volley5(Page):
+    form_model = 'group'
+    template_name = 'volleying/Volley.html'
+
+    def get_form_fields(self): 
+        return self.group.get_remaining_movies()
+
+    def before_next_page(self):
+        if self.timeout_happened:
+            self.player.set_timeout_data()
+            
+    def is_displayed(self):
+        return self.group.isVolleying
 
 class Results(Page):
-    pass
+    form_model = 'group'
+    template_name = 'volleying/Volley.html'
+
+    def get_form_fields(self): 
+        return self.group.get_remaining_movies()
+
+    def before_next_page(self):
+        if self.timeout_happened:
+            self.player.set_timeout_data()
+            
+    def is_displayed(self):
+        return self.group.isVolleying
 
 
 page_sequence = [
     Introduction,
     ParticipantInfo,
     Instructions,
-    Results
+    Chat,
+    Volley1,
+    Volley2,
+    Volley3,
+    Volley4,
+    Volley5
 ]
